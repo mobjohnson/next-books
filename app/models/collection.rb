89b13b878a3e23e5e30bbcc67e8802ef8609336b
mobjohnson <mobjohnson@gmail.com>
2014-09-08 12:11:50 -0400

    Add index view

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,8 +1,8 @@
 class Collection < ActiveRecord::Base
 	has_many :authors
 	belongs_to :user
-	has_many :books through: :author
+	has_many :books, through: :author
 
-	validates_associate :user
-	validates_associate :author
+	validates_associated :user
+	validates_associated :author
 end
