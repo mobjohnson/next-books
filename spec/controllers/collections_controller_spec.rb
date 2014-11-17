d2ddb4708f249002275e42aea9772437a3a2795d
mobjohnson <mobjohnson@gmail.com>
2014-11-17 13:16:31 -0500

    Add collections_controller_spec for 7 passing tests

---
+++ spec/controllers/collections_controller_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe CollectionsController, :type => :controller do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all of the collections into @collections" do
+      collection1, collection2 = Collection.create!, Collection.create!
+      get :index
+
+      expect(assigns(:collections)).to match_array([collection1, collection2])
+    end
+  end
+end
+
+
+# RSpec.describe PostsController, :type => :controller do
+#   describe "GET #index" do
+#     it "responds successfully with an HTTP 200 status code" do
+#       get :index
+#       expect(response).to be_success
+#       expect(response).to have_http_status(200)
+#     end
+
+#     it "renders the index template" do
+#       get :index
+#       expect(response).to render_template("index")
+#     end
+
+#     it "loads all of the posts into @posts" do
+#       post1, post2 = Post.create!, Post.create!
+#       get :index
+
+#       expect(assigns(:posts)).to match_array([post1, post2])
+#     end
+#   end
+# end
