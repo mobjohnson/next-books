aca82bcc56d50e877cee232005b301c1b9219415
mobjohnson <mobjohnson@gmail.com>
2014-11-17 16:39:26 -0500

    Include modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909185837) do
+ActiveRecord::Schema.define(version: 20141117212051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20140909185837) do
   end
 
   add_index "authors", ["collection_id"], name: "index_authors_on_collection_id", using: :btree
+
+  create_table "authors_books", id: false, force: true do |t|
+    t.integer "author_id", null: false
+    t.integer "book_id",   null: false
+  end
 
   create_table "books", force: true do |t|
     t.string  "title"
